bce444d7ee80ea4abd0517008544d94607d59bcc
Vishal Yadav <vishalyadav10014@gmail.com>
2026-07-24 09:30:37 +0530

    feat: add authentication service interface

---
+++ NewsPortalCMS.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using NewsPortalCMS.Application.DTOs.Auth;
+
+namespace NewsPortalCMS.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<string> RegisterAsync(RegisterDto model);
+
+    Task<string> LoginAsync(LoginDto model);
+}
---
+++ NewsPortalCMS.Application/Services/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using NewsPortalCMS.Application.DTOs.Auth;
+using NewsPortalCMS.Application.Interfaces;
+
+namespace NewsPortalCMS.Application.Services;
+
+public class AuthService : IAuthService
+{
+    public async Task<string> RegisterAsync(RegisterDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> LoginAsync(LoginDto model)
+    {
+        throw new NotImplementedException();
+    }
+}
