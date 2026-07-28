5085650498a7f5ff7f7b489a26ab4b244f10c4d5
Vishal Yadav <vishalyadav10014@gmail.com>
2026-07-28 16:16:20 +0530

    feat: complete tag CRUD module

---
+++ NewsPortalCMS.Application/DTOs/Tag/TagResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace NewsPortalCMS.Application.DTOs.Tag;
+
+public class TagResponseDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Slug { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+}
---
+++ NewsPortalCMS.Application/Interfaces/ITagService.cs
@@ -0,0 +1,16 @@
+﻿using NewsPortalCMS.Application.DTOs.Tag;
+
+namespace NewsPortalCMS.Application.Interfaces;
+
+public interface ITagService
+{
+    Task<List<TagResponseDto>> GetAllAsync();
+
+    Task<TagResponseDto?> GetByIdAsync(int id);
+
+    Task<TagResponseDto> CreateAsync(TagCreateDto model);
+
+    Task<bool> UpdateAsync(int id, TagUpdateDto model);
+
+    Task<bool> DeleteAsync(int id);
+}
