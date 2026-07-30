e397d3dd3e625099502d0b20f0cda374d7e7d79d
Uchit-Shubhangi <uchitshubhangi@gmail.com>
2026-07-30 17:14:44 +0530

    Add User Management module

---
+++ NewsPortalCMS.Application/Mappings/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NewsPortalCMS.Application.DTOs.User;
+using NewsPortalCMS.Domain.Entities;
+
+namespace NewsPortalCMS.Application.Mappings;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<ApplicationUser, UserResponseDto>();
+
+        CreateMap<UpdateUserDto, ApplicationUser>();
+    }
+}
---
+++ NewsPortalCMS.Application/Validators/User/UpdateUserStatusDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using NewsPortalCMS.Application.DTOs.User;
+
+namespace NewsPortalCMS.Application.Validators.User;
+
+public class UpdateUserStatusDtoValidator
+    : AbstractValidator<UpdateUserStatusDto>
+{
+    public UpdateUserStatusDtoValidator()
+    {
+        RuleFor(x => x.IsActive)
+            .NotNull();
+    }
+}
