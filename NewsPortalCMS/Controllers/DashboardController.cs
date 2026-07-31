b507777b6b449ac0ddcaa01e4c21c286b0c8df5d
Uchit-Shubhangi <uchitshubhangi@gmail.com>
2026-07-31 12:01:27 +0530

    Develop Dashboard module

---
+++ NewsPortalCMS/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NewsPortalCMS.Application.Interfaces.Services;
+
+namespace NewsPortalCMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] // Replace with your existing role/permission policy if required
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboardStatistics()
+        {
+            var result = await _dashboardService.GetDashboardStatisticsAsync();
+
+            return Ok(result);
+        }
+    }
+}
