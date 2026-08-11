c345479450725d091de204e17686434c13b4b1d0
Uchit-Shubhangi <uchitshubhangi@gmail.com>
2026-08-11 15:57:04 +0530

    AdvertisementProfile.cs file updated

--- NewsPortalCMS.Application/Mappings/AdvertisementProfile.cs
+++ NewsPortalCMS.Application/Mappings/AdvertisementProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using NewsPortalCMS.Application.DTOs.Advertisement;
 using NewsPortalCMS.Domain.Entities;
+using NewsPortalCMS.Domain.Enums;
 
 namespace NewsPortalCMS.Application.MappingProfiles
 {
@@ -8,8 +9,26 @@ namespace NewsPortalCMS.Application.MappingProfiles
     {
         public AdvertisementProfile()
         {
-            // Entity -> Response
-            CreateMap<Advertisement, AdvertisementResponseDto>();
+            CreateMap<CreateAdvertisementDto, Advertisement>();
+
+            CreateMap<UpdateAdvertisementDto, Advertisement>();
+
+            CreateMap<Advertisement, AdvertisementResponseDto>()
+                .ForMember(
+                    dest => dest.PositionName,
+                    opt => opt.MapFrom(src => GetPositionName(src.Position))
+                );
+        }
+
+        private static string GetPositionName(AdvertisementPosition position)
+        {
+            return position switch
+            {
+                AdvertisementPosition.TopBanner => "Top Banner",
+                AdvertisementPosition.Sidebar => "Sidebar",
+                AdvertisementPosition.Footer => "Footer",
+                _ => "Unknown"
+            };
         }
     }
 }
